007b9badb99aca73348e08e4684a9d841e6ef5c9
fliw <fliw@digitalkode.com>
2023-07-09 18:22:16 +0700

    chore: add middleware for admin role

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class isAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user()->is_admin) {
+            return $next($request);
+        }
+
+        return response('Unauthorized.', 401);
+    }
+}
