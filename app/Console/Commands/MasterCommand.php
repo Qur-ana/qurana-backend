78319fb5e0ed496e80001afd8a9e8590e9262473
fliw <fliw@digitalkode.com>
2023-07-13 18:59:58 +0700

    feat(kernel) : simplify command

--- app/Console/Commands/MasterCommand.php
+++ app/Console/Commands/MasterCommand.php
@@ -26,18 +26,43 @@ class MasterCommand extends Command
     public function handle()
     {
         echo "Setting up the application...\n";
+        echo "
+        ____                              
+        / __ \                             
+       | |  | |_   _ _ __ __ _ _ __   __ _ 
+       | |  | | | | | '__/ _` | '_ \ / _` |
+       | |__| | |_| | | | (_| | | | | (_| |
+         \___\_\\__,_|_|  \__,_|_| |_|\__,_|
+        \n";
+        echo "Assalamuaalaikum Wr. Wb.\n";
+        sleep(2);
+        echo "Hi Developers, Welcome to the application setup wizard of Qurana.\n";
+        sleep(2);
+        echo "This wizard will prepare your application for first use.\n";
+        sleep(2);
         echo "Generating application key...\n";
         $this->call('key:generate');
+        echo "Key generated!\n";
+        sleep(1);
         echo "Migrating database...\n";
         $this->call('migrate');
+        echo "Database migrated";
+        sleep(1);
         echo "Seeding database...\n";
         $this->call('db:seed');
+        echo "Database seeded!\n";
+        sleep(1);
         echo "Generating JWT secret...\n";
         $this->call('jwt:secret');
+        echo "JWT secret generated!\n";
+        sleep(1);
         echo "Fetching data Quran...\n";
         $this->call('app:fetch-quran');
+        echo "Fetching data Quran completed!\n";
+        sleep(1);
         echo "Fetching data City...\n";
         $this->call('app:fetch-city');
+        sleep(1);
         echo "Setup completed!\n";
     }
 }
